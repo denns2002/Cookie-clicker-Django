33f88f9ff1023b5f3879c04d5a31f81f516c1a2b
aiturgantalant <denis.israfilov2002@mail.ru>
2022-05-11 20:57:10 +0300

    reg, log

---
+++ cookieclicker/backend/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password_confirm')
+
+    username = forms.CharField(
+        max_length=20,
+        label='Your Username',
+        widget=forms.TextInput(attrs={'placeholder': 'Enter'}),
+    )
+
+    password = forms.CharField(
+        min_length=3,
+        max_length=20,
+        label='Your Password',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}),
+    )
+
+    password_confirm = forms.CharField(
+        min_length=3,
+        max_length=20,
+        label='Password Confirmation',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat password'}),
+    )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password == password_confirm:
+            return cleaned_data
+        raise forms.ValidationError('Passwords not equals each other~!<3')
+
+    def safe(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data['password']
+        user.set_password(password)
+        if commit:
+            user.save()
+        return user
--- cookieclicker/backend/views.py
+++ cookieclicker/backend/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            login(request, user)
+            return redirect('index')
+        return render(request, 'register.html', {'user_form': user_form})
+
+    user_form = UserForm()
+    return render(request, 'register.html', {'user_form': user_form})
+
+
+def user_login(request):
+    user_form = UserForm()
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST.get('username'),
+            password=request.POST.get('password')
+        )
+        if user:
+            login(request, user)
+            return redirect('index')
+
+        return render(request, 'login.html', {
+            'user_form': user_form, 'invalid': True
+        })
+
+    return render(request, 'login.html', {'user_form': user_form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
